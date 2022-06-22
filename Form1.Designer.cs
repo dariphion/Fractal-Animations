6a32c2c84aed0c2aa4bdc9729a61856b641edbcf
user <user@WIN-FSEUVMKQQM0>
2022-06-22 17:55:28 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace idk_for_now
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textbox_line_num = new System.Windows.Forms.TextBox();
+            this.textbox_length = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textbox_angle = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.SystemColors.Highlight;
+            this.TopPanel.Controls.Add(this.button7);
+            this.TopPanel.Controls.Add(this.button6);
+            this.TopPanel.Controls.Add(this.button5);
+            this.TopPanel.Controls.Add(this.button4);
+            this.TopPanel.Controls.Add(this.button3);
+            this.TopPanel.Controls.Add(this.button2);
+            this.TopPanel.Controls.Add(this.label4);
+            this.TopPanel.Controls.Add(this.label2);
+            this.TopPanel.Controls.Add(this.textbox_line_num);
+            this.TopPanel.Controls.Add(this.textbox_length);
+            this.TopPanel.Controls.Add(this.label1);
+            this.TopPanel.Controls.Add(this.textbox_angle);
+            this.TopPanel.Controls.Add(this.button1);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1904, 83);
+            this.TopPanel.TabIndex = 0;
+            this.TopPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "# of Lines";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Length";
+            // 
+            // textbox_line_num
+            // 
+            this.textbox_line_num.Location = new System.Drawing.Point(57, 57);
+            this.textbox_line_num.Name = "textbox_line_num";
+            this.textbox_line_num.Size = new System.Drawing.Size(65, 20);
+            this.textbox_line_num.TabIndex = 5;
+            // 
+            // textbox_length
+            // 
+            this.textbox_length.Location = new System.Drawing.Point(57, 31);
+            this.textbox_length.Name = "textbox_length";
+            this.textbox_length.Size = new System.Drawing.Size(65, 20);
+            this.textbox_length.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Angle";
+            // 
+            // textbox_angle
+            // 
+            this.textbox_angle.Location = new System.Drawing.Point(57, 5);
+            this.textbox_angle.Name = "textbox_angle";
+            this.textbox_angle.Size = new System.Drawing.Size(65, 20);
+            this.textbox_angle.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Save Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(138, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 83);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(1904, 958);
+            this.canvas.TabIndex = 1;
+            this.canvas.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(232, 38);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Rotating animation";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(232, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Vortex animation";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(351, 38);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(113, 23);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Growth animation";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(351, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(113, 23);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Zoom animation";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.TopPanel);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fractals Visualisation";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textbox_angle;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textbox_line_num;
+        private System.Windows.Forms.TextBox textbox_length;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace idk_for_now
+{
+    public partial class Form1 : Form
+    {
+        Pen myPen = new Pen(Color.Black);
+        Graphics g;
+        Bitmap bm;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            bm = new Bitmap(canvas.Width, canvas.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            canvas.Image = bm;
+
+            textbox_angle.Text = "59";
+            textbox_length.Text = "0";
+            textbox_line_num.Text = "1500";
+        }
+
+        //"Draw" button
+        public void button1_Click(object sender, EventArgs e) 
+        {
+            g.Dispose();
+            bm = new Bitmap(canvas.Width, canvas.Height);
+            g = Graphics.FromImage(bm);
+            g.Clear(Color.White);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int start_x = canvas.Width / 2;
+            int start_y = canvas.Height / 2;
+            int length = Int32.Parse(textbox_length.Text);
+            int line_num = Int32.Parse(textbox_line_num.Text);
+            int angle = Int32.Parse(textbox_angle.Text);
+            int current_angle = 0;
+
+            myPaint(start_x, start_y, length, angle, line_num, current_angle);
+            
+        }
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void canvas_Paint(object sender, PaintEventArgs e) 
+        {
+
+        }
+
+        //"Save" button
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            bm.Save(desktop + "/TestImage.jpeg", ImageFormat.Jpeg);
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            canvas.Image = bm;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            g.Dispose();
+            bm = new Bitmap(canvas.Width, canvas.Height);
+            g = Graphics.FromImage(bm);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int start_x = canvas.Width / 2;
+            int start_y = canvas.Height / 2;
+            int length = Int32.Parse(textbox_length.Text);
+            int line_num = Int32.Parse(textbox_line_num.Text);
+            int angle = Int32.Parse(textbox_angle.Text);
+            for (int i=0; i<360; i++)
+            {
+                myPaint(start_x, start_y, length, angle, line_num, i);
+            }
+            
+        }
+
+        public void myPaint(int start_x, int start_y, int length, int angle, int line_num, int start_angle)
+        {
+            g.Clear(Color.White);
+            int current_angle = start_angle;
+            for (int i = 0; i < line_num; i++)
+            {
+                int end_x = (int)(start_x + Math.Cos(current_angle * Math.PI / 180) * length);
+                int end_y = (int)(start_y + Math.Sin(current_angle * Math.PI / 180) * length);
+
+                Point[] points =
+                {
+                    new Point(start_x, start_y),
+                    new Point(end_x, end_y)
+
+                };
+
+
+                g.DrawLines(myPen, points);
+
+                start_x = end_x;
+                start_y = end_y;
+                current_angle += angle;
+                length += 1;
+            }
+            canvas.Image = bm;
+            canvas.Refresh();
+        }
+
+        
+            
+        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            g.Dispose();
+            bm = new Bitmap(canvas.Width, canvas.Height);
+            g = Graphics.FromImage(bm);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int start_x = canvas.Width / 2;
+            int start_y = canvas.Height / 2;
+            int length = Int32.Parse(textbox_length.Text);
+            int line_num = Int32.Parse(textbox_line_num.Text);
+            int current_angle = 0;
+            for (int i = 0; i < 360; i++)
+            {
+                myPaint(start_x, start_y, length, i, line_num, current_angle);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            g.Dispose();
+            bm = new Bitmap(canvas.Width, canvas.Height);
+            g = Graphics.FromImage(bm);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int start_x = canvas.Width / 2;
+            int start_y = canvas.Height / 2;
+            int length = Int32.Parse(textbox_length.Text);
+            int angle = Int32.Parse(textbox_angle.Text);
+            int current_angle = 0;
+            int line_num = Int32.Parse(textbox_line_num.Text);
+            int increment;
+           
+                for (int i = 0; i < line_num; i=i+increment)
+                {
+                    myPaint(start_x, start_y, length, angle, i, current_angle);
+                    increment = (i / 50)+1;
+                }
+               
+            
+            
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            g.Dispose();
+            bm = new Bitmap(canvas.Width, canvas.Height);
+            g = Graphics.FromImage(bm);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int start_x = canvas.Width / 2;
+            int start_y = canvas.Height / 2;
+            int angle = Int32.Parse(textbox_angle.Text);
+            int line_num = Int32.Parse(textbox_line_num.Text);
+            int current_angle = 0;
+            for (int i = 0; i < 500; i=i+12)
+            {
+                myPaint(start_x, start_y, i, angle, line_num, current_angle);
+            }
+        }
+    }
+    
+}
